eb75c01d0eefd393935c4cac7877187a6e36eea5
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2022-02-21 23:20:55 +0100

    fix: added new component lib

---
+++ components/test-account/dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { TestAccountSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  TestAccountSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <test-account-sample />
+  </div>
+</template>
---
+++ components/test-account/src/lib-components/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as TestAccountSample } from './test-account-sample.vue';
